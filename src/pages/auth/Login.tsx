dd77dca2e0b3c3d068bd0f6f7e46a370f1e2d101
filipeavila7 <filipeavila076@gmail.com>
2026-08-25 10:38:33 -0300

    iniciando designer da tela de login

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,8 +1,27 @@
-
+import "../../styles/login.css"
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className="login-lay">
+      <div className="login-box-left">
+        <div className="title-r-box">
+          <h1>Share.</h1>
+          <h1>Get inspired.</h1>
+          <h1>Connect.</h1>
+          <div className="p-r-box">
+            <p>A platform for artists and fans to share ideas, creations, and stories</p>
+          </div>
+          
+        </div>
+
+      </div>
+      <div className="login-box-right">
+        <h1>Login</h1>
+        <div className="login-box">
+            
+        </div>
+      </div>
+    </div>
   )
 }
 
