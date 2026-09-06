679d8b72a21126064228896ff9a9d29eb7980383
filipeavila7 <filipeavila076@gmail.com>
2026-09-06 11:30:36 -0300

    empty posts

--- src/components/post/EmptyPosts.tsx
+++ src/components/post/EmptyPosts.tsx
@@ -1,7 +1,18 @@
+import { FaPlus } from "react-icons/fa6"
+import "../../styles/post.css"
+
 
 function EmptyPosts() {
   return (
-    <div></div>
+    <div className="empty-post-box">
+      <img className="empty-avatar" src="/avatata-2.png" alt="" />
+      <div className="empty-title">
+        <h1>No posts yet!</h1>
+        <h3>Start sharing something and let your profile come to life.</h3>
+        <button className="empty-btn"><FaPlus/>   New post</button>
+      </div>
+      
+    </div>
   )
 }
 
